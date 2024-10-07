1cc35c38cdd0202fb8464a562dbed06bc3f6129e
Parth Sharma <bhawanal1978@gmail.com>
2024-10-07 19:53:04 +0530

    Add new files

--- ArrowFunctions/arrowFunction.js
+++ ArrowFunctions/arrowFunction.js
@@ -14,7 +14,6 @@ console.log(c);
 hello()
 
 // Implicit Return (agar sirf return karna ho arrow function mein toh return likhne ki zaroorat nahi hai alag syntax hota hai)
-const mul = (a, b) =>(
-    a*b
-)
+// curly ki jagah parenthisis hojaayega
+const mul = (a, b) => a*b
 console.log(mul(2, 3));
--- ArrowFunctions/setTimeout.js
+++ ArrowFunctions/setTimeout.js
@@ -1,7 +1,9 @@
 console.log("Hi there!");
-// setTimeout(function, time)
+// setTimeout(function, time) time in milisecond
 setTimeout(() => {
     console.log("JavaScript");
 }, 4000);
 
 console.log("Welcome to");
+// setTimeout does not stops the program the program goes on executing and after
+// given time setTimeout will execute.
