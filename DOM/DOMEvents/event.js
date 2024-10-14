85919cdce49a297068b9c00f007703efdd02cdb8
Parth Sharma <bhawanal1978@gmail.com>
2024-10-14 22:54:42 +0530

    DOM Events completd

--- DOM/DOMEvents/event.js
+++ DOM/DOMEvents/event.js
@@ -1,8 +1,8 @@
 let form = document.querySelector("form");
 form.addEventListener("submit", function(event){
     event.preventDefault(); // Default operation is prevented (action waala submit ke baad action waale url pe nahi jaayega)
-    let user = this.elements[0];
+    let user = this.elements[0]; // or form.elements[0]
     let pass = this.elements[1];
     console.log(user.value);
     console.log(pass.value);
-})
+});
