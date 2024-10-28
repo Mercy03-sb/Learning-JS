efeeac73890b1fe352d8019a7b8bbc3dca067f26
Parth Sharma <bhawanal1978@gmail.com>
2024-10-28 16:52:57 +0530

    API added

--- API(Application Development Interface)/app.js
+++ API(Application Development Interface)/app.js
@@ -18,3 +18,14 @@ async function getFacts() {
     return "No fact found";
   }
 }
+// Normally for this api we get data in html format to get the data in json format we have to pass a header in get request.
+const url2 = "https://icanhazdadjoke.com/";
+async function getJokes(params) {
+  try {
+    const config = { headers: { Accept: "application/json" } }; // To get data in json format directly
+    let res = await axios.get(url, config);
+    console.log(res.data);
+  } catch (err) {
+    console.log("Error:", err);
+  }
+}
